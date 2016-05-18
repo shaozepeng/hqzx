9e87fce2ecc90bb24405de55fe4caec857f7c390
shaozp <shaozepeng_2008@126.com>
2016-05-18 21:08:09 +0800

    Initial Commit

---
+++ hqzx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hqzx
+//
+//  Created by 泽鹏邵 on 16/5/18.
+//  Copyright © 2016年 泽鹏邵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
